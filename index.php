4eec0db79a0c4e843cc8b6fbf6faebaebfc892a4
Mika RAMPARANY <migame.strass@gmail.com>
2023-02-15 12:53:03 +0100

    ajout nouvel auteur pour tester l'index

--- index.php
+++ index.php
@@ -13,10 +13,21 @@ spl_autoload_register(function ($class_name)
     require_once $class_name . '.php';
 });
 
+// STEPHEN KING Bibliographie : 
 $A1 = new Auteurs ("Stephen", "King");
-$L1A1 = new Livres ( "Ca",1138,1986,"20",$A1);
+$L1A1 = new Livres ( "Ça",1138,1986,20,$A1);
 $L2A1= new Livres ( "Simetierre",374,1983,15,$A1);
 $L3A1= new Livres ( "Le Fléau", 1978, 823, 14, $A1);
 $L4A1 = new Livres ( "Shining",1977, 447, 16, $A1);
 
 $A1 -> afficherBibliographie();
+
+// HARLAN COBEN Bibliographie : 
+
+$A2 = new Auteurs ("Harlan", "Coben");
+$L1A2 = new Livres ( "L'Inconnu de la forêt", 480, 2021, 20.99, $A2);
+$L2A2 = new Livres ( "Six ans déjà", 368, 2015, 12.99, $A2);
+$L3A2 = new Livres ( "Identités croisées", 397, 2022, 22.90, $A2);
+$L4A2 = new Livres ( "Ne le dis à personne", 431, 2001, 8.99, $A2 );
+
+$A2 -> afficherBibliographie();
